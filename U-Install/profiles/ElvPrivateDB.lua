c0911833cf3824e76e607bdbb927d0392386db5b
Josh Russell <JRussell20@gmail.com>
2013-01-02 02:55:23 -0500

    General settings.

--- U-Install/profiles/ElvPrivateDB.lua
+++ U-Install/profiles/ElvPrivateDB.lua
@@ -2,13 +2,24 @@ local E, L, C = unpack(select(2, ...))
 
 local name, ace3 = "ElvPrivateDB", true
 local profile = {
-  ["skins"] = {
-    ["addons"] = {
-      ["EmbedSkada"] = true,
-      ["AlwaysTrue"] = true,
-    },
-  },
-  ["install_complete"] = "5.1",
-}
+   		["general"] = {
+			["normTex"] = "Flatt",
+			["glossTex"] = "Flatt",
+		},
+		['auras'] = {
+			['enable'] = true,
+			['size'] = 28,
+		},	
+		["skins"] = {
+			["addons"] = {
+				["EmbedRight"] = true,
+				["EmbedRecount"] = true,
+				["EmbedSkada"] = true,
+				["AlwaysTrue"] = true,
+				["EmbedOoC"] = true,
+			},
+		},
+		["install_complete"] = "5.1",
+	}
 
 E:RegisterProfile(name, profile, ace3)
